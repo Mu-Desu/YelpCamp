50c22023c5984b3d18148e1d084dd8669a11bb33
Mu Yaser <mu.yaser@hotmail.com>
2022-06-24 19:57:26 +0300

    changed how mongoDBStore works

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const userRoutes = require('./routes/user')
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews');
 
-const MongoDBStore = require('connect-mongo')(session);
+const MongoDBStore = require('connect-mongo')
 
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
@@ -47,7 +47,7 @@ app.use(mongoSanitize())
 
 const secret = process.env.SECRET || 'meowmeowthecatsounduwu';
 
-const store = new MongoDBStore({
+const store = new MongoDBStore.create({
     url: dbUrl,
     secret: secret,
     touchAfter: 24 * 60 * 60
